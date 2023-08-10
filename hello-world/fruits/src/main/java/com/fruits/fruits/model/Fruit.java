fac6e03354d68bece7f8f6602bc78e8279926f04
64bitmachine <85003035+rathoddinesh14@users.noreply.github.com>
2023-08-10 20:34:44 +0530

    added spring boot backend server

---
+++ hello-world/fruits/src/main/java/com/fruits/fruits/model/Fruit.java
@@ -0,0 +1,50 @@
+package com.fruits.fruits.model;
+
+public class Fruit {
+    private String name;
+    private String color;
+    private String size;
+    private float price;
+    private String shape;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+
+    public void setShape(String shape) {
+        this.shape = shape;
+    }
+
+}
